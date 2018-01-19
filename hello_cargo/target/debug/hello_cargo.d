e2b5dc3d9c7c5486f884a97f37f208176bf8c679
Lei Zhao <leizmonk@gmail.com>
2018-01-18 22:19:16 -0500

    some cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/leizmonk/src/learn-rust/hello_cargo/target/debug/hello_cargo: /home/leizmonk/src/learn-rust/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/leizmonk/src/learn-rust/hello_cargo/target/release/hello_cargo: /home/leizmonk/src/learn-rust/hello_cargo/src/main.rs
