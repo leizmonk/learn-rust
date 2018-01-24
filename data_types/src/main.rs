80ef002df4b62477cddd14a317f00b6e4d599152
Lei Zhao <lei@petflow.com>
2018-01-24 09:56:30 -0500

    data types

---
+++ data_types/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let _x = 2.0; // f64, 64-bit float, default float type
+    let _y: f32 = 3.0; // f32, 32-bit float, must be specified
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let remainder = 43 % 5;
+
+    println!("Sum is {}", sum);
+    println!("Difference is {}", difference);
+    println!("Product is {}", product);
+    println!("Quotient is {}", quotient);
+    println!("Remainder is {}", remainder);
+}
---
+++ data_types/target/debug/data_types.d
@@ -0,0 +1,1 @@
+/Users/leizhao/src/learn-rust/data_types/target/debug/data_types.dSYM: /Users/leizhao/src/learn-rust/data_types/src/main.rs
