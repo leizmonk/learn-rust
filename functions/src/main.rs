d59b4868985536560a5d6d71f838d59458d6e3b8
Lei Zhao <lei@petflow.com>
2018-01-26 16:31:56 -0500

    fun w fns

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(3, 1);
+    let res = try_this(5);
+    println!("The value of try_this(5) is: {}", res);    
+}
+
+fn another_function(x: i32, y: i64) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
+
+fn try_this(a: i32) -> i32 {
+    a + 1
 }
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/leizhao/src/learn-rust/functions/target/debug/functions.dSYM: /Users/leizhao/src/learn-rust/functions/src/main.rs
