237435e41123f368f7730f774993eb14658d5b3f
Lei Zhao <lei@petflow.com>
2018-02-12 15:08:25 -0500

    pig latin?

--- try_hashes/src/pig_latin.rs
+++ try_hashes/src/pig_latin.rs
@@ -4,8 +4,8 @@ pub fn pig_latinize(input: &str) {
     let vowels = ["a", "e", "i", "o", "u", "y"];
     let mut pigged = HashMap::new();
 
-    for w in input.split_whitespace() {
-        pigged.insert(w, w.chars().next().unwrap());
+    for w in input.split_whitespace() { 
+        pigged.insert(w, w.chars().next().unwrap().to_string());
     }
 
     println!("{:?}", pigged);
